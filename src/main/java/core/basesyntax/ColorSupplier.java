98c2054b964449247d14451225bc4238dea50c70
VadimN637 <turnedofff@gmail.com>
2026-02-09 20:20:32 +0200

    2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    public enum Colors {
+        RED,
+        YELLOW,
+        WHITE,
+        BLACK,
+        BLUE
+    }
+
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
